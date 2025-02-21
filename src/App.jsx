d70460efbd9380c93623219c0f4e4ac76bfc6a71
PJ <pjloury@gmail.com>
2025-02-21 00:22:37 -0800

    Added logos to the field and added super bowl teams

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="team chiefs">
+        <h1>CHIEFS</h1>
+      </div>
+      <div className="team eagles">
+        <h1>EAGLES</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App 
